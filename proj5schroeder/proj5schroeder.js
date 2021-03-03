52d42e12e90b7d81dc8a1c1a158adf2f2eac07e7
vir_ago <amelia.schroeder62@gmail.com>
2021-03-03 15:27:01 -0600

    finished for loop alert message system

--- proj5schroeder/proj5schroeder.js
+++ proj5schroeder/proj5schroeder.js
@@ -1,17 +1,42 @@
-function init() {
-    var fName = document.orderForm.firstName.value
+//function that checks that all required input fields are filled and displays a error message if they are not
+function validateForm(e) {
 
-    if (fName == "") {
-        var fNameDiv = document.getElementById("fnInput");
+    //gets all the instances of class="required" (all in text input elements)
+    var requiredField = document.getElementsByClassName("required");
 
-        var alertP = document.createElement(p);
-        var node = document.createTextNode("Please Enter a Name");
-        alertP.appendChild(node);
+    //gets the divs of all the required input fields
+    var rInputDivs = document.getElementsByClassName("reqInputDiv");
 
-        fNameDiv.appendChild(alertP);
+    //creates array from how many class = "required" instances there 
+    var len = Array.from(requiredField).length;
 
-        //prevents submission
-        e.preventDefault();
+    for (let i = 0; i < len; i++) {
+        //gets values of required fields as deliniated by class name
+        var rFieldValue = requiredField[i].value;
+
+        //gets divs of required inputs
+        var rInputDiv = rInputDivs[i];
+
+        if (rFieldValue == "") {
+            //creates a p element to put the alert in
+            var alertP = document.createElement("p");
+            //creates text inside p element
+            var node = document.createTextNode("Field is Required");
+            //puts the text into the p
+            alertP.appendChild(node);
+
+            rInputDiv.appendChild(alertP);
+
+            //prevents submission
+            e.preventDefault();
+        }
     }
-
 }
+
+
+
+function init() {
+    var f = document.getElementsByName("orderForm");
+    f[0].addEventListener("submit", validateForm);
+}
+window.onload = init;
