295390916b579c5dddb5b46a32a3657869a641bb
vir_ago <amelia.schroeder62@gmail.com>
2021-03-03 21:25:10 -0600

    functionally done, added regular expression to email section checking for @ symbol

--- proj5schroeder/proj5schroeder.js
+++ proj5schroeder/proj5schroeder.js
@@ -1,11 +1,7 @@
 //function that checks that all required input fields are filled and displays a error message if they are not
 function validateForm(e) {
-
     //gets all the instances of class="required" (all in text input elements)
     var requiredField = document.getElementsByClassName("required");
-
-    //gets the divs of all the required input fields
-    var rInputDivs = document.getElementsByClassName("reqInputDiv");
 
     //creates array from how many class = "required" instances there 
     var len = Array.from(requiredField).length;
@@ -14,50 +10,69 @@ function validateForm(e) {
         //gets values of required fields as deliniated by class name
         var rFieldValue = requiredField[i].value;
 
-        //gets divs of required inputs
-        var rInputDiv = rInputDivs[i];
+        var alertP = document.getElementsByClassName("alertP");
 
         if (rFieldValue == "") {
-            //creates a p element to put the alert in
-            var alertP = document.createElement("p");
-            //creates text inside p element
-            var node = document.createTextNode("Field is Required");
-            //puts the text into the p
-            alertP.appendChild(node);
-
-            rInputDiv.appendChild(alertP);
+            alertP[i].style.display = "inline";
 
             //prevents submission
             e.preventDefault();
+        } else {
+            alertP[i].style.display = "none";
         }
     }
 }
 
+
 function showDelInstruct(chk) {
-
-
+    //gets checkbox element
     var chk = document.getElementById("dInstrucChk");
+
+    //gets delivery instruction input box element 
     var dInstruc = document.getElementById("dInstruc");
+
+    //gets delivery instruction label element
     var dInstrucLabel = document.getElementById("dInstrucLabel");
 
+    //shows the optional delivery instructions if the checkbox is checked
     if (chk.checked == true) {
-
         dInstruc.style.display = "inline";
         dInstrucLabel.style.display = "inline";
-
-
     } else {
         dInstruc.style.display = "none";
         dInstrucLabel.style.display = "none";
 
     }
+}
 
+function checkEmail(e) {
+    var email = document.getElementById("email").value;
+    var re1 = new RegExp("@");
+    var emailAlertP = document.getElementById("emailAlertP");
+    if (re1.test(email) == false) {
+        emailAlertP.innerHTML = "Please enter a valid Email address";
+        emailAlertP.style.display = "inline";
+        e.preventDefault();
+    }
 }
 
 function init() {
+
+    //hides all of the error messages 
+    var requiredField = document.getElementsByClassName("required");
+    var alertP = document.getElementsByClassName("alertP");
+    var len = Array.from(requiredField).length;
+    for (let i = 0; i < len; i++) {
+        alertP[i].style.display = "none";
+    }
+
+    //gets form element and adds event listener to submit button
     var f = document.getElementsByName("orderForm");
     f[0].addEventListener("submit", validateForm);
+    f[0].addEventListener("submit", checkEmail);
 
+
+    //runs the show optional delivery instructions function
     showDelInstruct();
 
 }
